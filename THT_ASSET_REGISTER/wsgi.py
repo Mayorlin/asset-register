1853c6f9cabf8e95f4eec9efac2c6e7eb19036f2
Gabriel <oyemomigabriel@gmail.com>
2026-02-09 14:11:57 +0100

    Fix Django version for Python 3.11

--- THT_ASSET_REGISTER/wsgi.py
+++ THT_ASSET_REGISTER/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "THT_ASSET_REGISTER.settings")
 
 application = get_wsgi_application()
+
+
+#SuperUser Creation (One-Time Use)
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'gabriel.oyemomi', 'Oluwamayowagabriel@2002')
