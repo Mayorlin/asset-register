98656f151e91848af7eb7cbfac656efe6d700b53
Gabriel <oyemomigabriel@gmail.com>
2026-02-09 14:26:00 +0100

    build.sh Modification

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+# The '|| true' ensures the build doesn't fail if the user already exists
+if [ "$CREATE_SUPERUSER" ]; then
+  python manage.py createsuperuser --noinput || true
+fi
